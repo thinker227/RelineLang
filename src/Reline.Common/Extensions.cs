4742e5d5226316cd032646ea398c091948ec2b4a
thinker227 <thinker0227@gmail.com>
2021-12-27 14:54:52 +0100

    Added non-generic IViewer
    For the sake of simplifying classes implementing multiple generic version of IViewer<T>

---
+++ src/Reline.Common/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Reline.Common;
+
+public static class Extensions {
+
+	public static IEnumerable<T> AsEnumerable<T>(this ImmutableArray<T> immutableArray) {
+		foreach (T element in immutableArray) yield return element;
+	}
+
+}
--- src/Reline.Common/IViewer.cs
+++ src/Reline.Common/IViewer.cs
@@ -5,17 +5,8 @@ namespace Reline.Common;
 /// <summary>
 /// Represents a viewer of a sequence.
 /// </summary>
-/// <typeparam name="T">The type of the elements in the sequence.</typeparam>
-public interface IViewer<out T> : IEnumerable<T> {
+public interface IViewer {
 
-	/// <summary>
-	/// The current element.
-	/// </summary>
-	T Current { get; }
-	/// <summary>
-	/// The next element.
-	/// </summary>
-	T Next { get; }
 	/// <summary>
 	/// Whether the viewer is at the end of the sequence.
 	/// </summary>
@@ -25,5 +16,22 @@ public interface IViewer<out T> : IEnumerable<T> {
 	/// Moves the viewer forward one element.
 	/// </summary>
 	void MoveNext();
+
+}
+
+/// <summary>
+/// Represents a viewer of a generic sequence.
+/// </summary>
+/// <typeparam name="T">The type of the elements in the sequence.</typeparam>
+public interface IViewer<out T> : IViewer, IEnumerable<T> {
+
+	/// <summary>
+	/// The current element.
+	/// </summary>
+	T Current { get; }
+	/// <summary>
+	/// The next element.
+	/// </summary>
+	T Next { get; }
 
 }
