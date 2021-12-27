702c1a0c273f86a6dc7ee80c2d190f284efe4067
thinker227 <thinker0227@gmail.com>
2021-12-27 11:55:15 +0100

    Create CompilationException.cs

---
+++ src/Reline.Compilation/CompilationException.cs
@@ -0,0 +1,24 @@
+﻿namespace Reline.Compilation;
+
+/// <summary>
+/// An exception caused by compilation.
+/// </summary>
+public class CompilationException : Exception {
+
+	/// <summary>
+	/// Initializes a new <see cref="CompilationException"/> instance.
+	/// </summary>
+	public CompilationException() { }
+	/// <summary>
+	/// Initializes a new <see cref="CompilationException"/> instance.
+	/// </summary>
+	/// <param name="message">The message describing the exception.</param>
+	public CompilationException(string? message) : base(message) { }
+	/// <summary>
+	/// Initializes a new <see cref="CompilationException"/> instance.
+	/// </summary>
+	/// <param name="message">The message describing the exception.</param>
+	/// <param name="inner">The inner exception which caused the current exception.</param>
+	public CompilationException(string? message, Exception? inner) : base(message, inner) { }
+
+}
