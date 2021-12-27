91fd0467351bf2513ffb45499235b8798b41fe3e
thinker227 <thinker0227@gmail.com>
2021-12-27 11:54:43 +0100

    Added IOperationResult

---
+++ src/Reline.Compilation/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Reline.Compilation.Diagnostics;
+
+namespace Reline.Compilation;
+
+public static class Extensions {
+
+	/// <summary>
+	/// Returns whether a <see cref="IOperationResult{T}"/> has any fatal errors.
+	/// </summary>
+	/// <typeparam name="T">The type of the result of the
+	/// <see cref="IOperationResult{T}"/>.</typeparam>
+	/// <param name="operationResult">The source operation result.</param>
+	/// <returns>Whether <paramref name="operationResult"/> contains any diagnostics with
+	/// a severity level of <see cref="DiagnosticLevel.Error"/>.</returns>
+	public static bool HasErrors<T>(this IOperationResult<T> operationResult) =>
+		operationResult.Diagnostics.Any(d => d.Level == DiagnosticLevel.Error);
+
+}
---
+++ src/Reline.Compilation/IOperationResult.cs
@@ -0,0 +1,20 @@
+﻿using Reline.Compilation.Diagnostics;
+
+namespace Reline.Compilation;
+
+/// <summary>
+/// Represents the result of a compilation operation.
+/// </summary>
+/// <typeparam name="T">The type of the result of the operation.</typeparam>
+public interface IOperationResult<T> {
+
+	/// <summary>
+	/// The diagnostics produced by the operation.
+	/// </summary>
+	ImmutableArray<Diagnostic> Diagnostics { get; }
+	/// <summary>
+	/// The result of the operation.
+	/// </summary>
+	T Result { get; }
+
+}
