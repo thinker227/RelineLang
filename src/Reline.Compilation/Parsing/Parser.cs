282d6604e4bdf36224ed514d41fed8ac95a73c8a
thinker227 <thinker0227@gmail.com>
2021-12-29 20:07:52 +0100

    Statement parsing implemented

--- src/Reline.Compilation/Parsing/Parser.cs
+++ src/Reline.Compilation/Parsing/Parser.cs
@@ -61,28 +61,76 @@ public sealed class Parser {
 		if (!viewer.MatchTypePattern(SyntaxType.NewlineToken))
 			statement = Statement();
 
-		var newlineToken = CheckType(SyntaxType.NewlineToken, SyntaxType.EndOfFile);
-		viewer.Advance();
+		var newlineToken = ExpectTypeAdvance(SyntaxType.NewlineToken, SyntaxType.EndOfFile);
 
 		return new(label, statement, newlineToken);
 	}
+
 	private IStatementSyntax Statement() {
+		if (viewer.CheckType(SyntaxType.MoveKeyword))
+			return MoveStatement();
+		if (viewer.CheckType(SyntaxType.SwapKeyword))
+			return SwapStatement();
+		if (viewer.CheckType(SyntaxType.CopyKeyword))
+			return CopyStatement();
+		if (viewer.MatchTypePattern(SyntaxType.Identifier, SyntaxType.EqualsToken))
+			return AssignmentStatement();
+
+		return ExpressionStatement();
+	}
+	private AssignmentStatementSyntax AssignmentStatement() {
+		var identifier = viewer.AdvanceCurrent();
+		var equalsToken = viewer.AdvanceCurrent();
+		var expression = Expression();
+		return new(new(identifier), equalsToken, expression);
+	}
+	private MoveStatementSyntax MoveStatement() {
+		var moveKeyword = viewer.AdvanceCurrent();
+		var source = Expression();
+		var toKeyword = ExpectTypeAdvance(SyntaxType.ToKeyword);
+		var target = Expression();
+		return new(moveKeyword, source, toKeyword, target);
+	}
+	private SwapStatementSyntax SwapStatement() {
+		var swapKeyword = viewer.AdvanceCurrent();
+		var source = Expression();
+		var withKeyword = ExpectTypeAdvance(SyntaxType.WithKeyword);
+		var target = Expression();
+		return new(swapKeyword, source, withKeyword, target);
+	}
+	private CopyStatementSyntax CopyStatement() {
+		var copyKeyword = viewer.AdvanceCurrent();
+		var source = Expression();
+		var toKeyword = ExpectTypeAdvance(SyntaxType.ToKeyword);
+		var target = Expression();
+		return new(copyKeyword, source, toKeyword, target);
+	}
+	private ExpressionStatementSyntax ExpressionStatement() {
+		var expression = Expression();
+		if (expression is not FunctionInvocationExpressionSyntax) {
+			Diagnostic diagnostic = new(DiagnosticLevel.Error, "Only function invocation expressions can be used as expression statements.", TextSpan.Empty);
+			diagnostics.Add(diagnostic);
+		}
+		return new(expression);
+	}
+
+	private IExpressionSyntax Expression() {
 		return null!;
 	}
 
-	private SyntaxToken CheckType(SyntaxType expected) {
+	private SyntaxToken ExpectTypeAdvance(SyntaxType expected) {
 		if (!viewer.CheckType(expected))
 			CreateDiagnosticAndSynchrnoize(new[] { expected });
-		return viewer.Current;
+		return viewer.AdvanceCurrent();
 	}
-	private SyntaxToken CheckType(params SyntaxType[] expected) {
+	private SyntaxToken ExpectTypeAdvance(params SyntaxType[] expected) {
 		if (!viewer.CheckType(expected))
 			CreateDiagnosticAndSynchrnoize(expected);
-		return viewer.Current;
+		return viewer.AdvanceCurrent();
 	}
-	private void CreateDiagnosticAndSynchrnoize(SyntaxType[] expected) {
+	private void CreateDiagnosticAndSynchrnoize(params SyntaxType[] expected) {
 		var current = viewer.Current;
-		string diagnosticText = $"Unexpected token '{current.Text}' at position {current.Span}";
+		string diagnosticText = $"Unexpected token {current.Type} at position {current.Span}";
 		Diagnostic diagnostic = new(DiagnosticLevel.Error, diagnosticText, current.Span);
 		diagnostics.Add(diagnostic);
 
