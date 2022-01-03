95de1a2f8f5f6c04891eb9cfdd5af913ec3ada0e
thinker227 <thinker0227@gmail.com>
2022-01-03 01:35:54 +0100

    Added comments to parser

--- src/Reline.Compilation/Parsing/Parser.cs
+++ src/Reline.Compilation/Parsing/Parser.cs
@@ -50,6 +50,7 @@ public sealed class Parser {
 		return new(lines.ToImmutableArray());
 	}
 	private LineSyntax Line() {
+		// Try find a label, otherwise the label is null
 		LabelSyntax? label = null;
 		if (viewer.MatchTypePattern(SyntaxType.Identifier, SyntaxType.ColonToken)) {
 			var identifier = viewer.AdvanceCurrent();
@@ -57,6 +58,7 @@ public sealed class Parser {
 			label = new(new(identifier), colonToken);
 		}
 
+		// Try find a statement, otherwise the statement is null
 		IStatementSyntax? statement = null;
 		if (!SyntaxRules.CanEndLine(viewer.Current.Type))
 			statement = Statement();
@@ -67,15 +69,20 @@ public sealed class Parser {
 	}
 
 	private IStatementSyntax Statement() {
+		// Move statement
 		if (viewer.CheckType(SyntaxType.MoveKeyword))
 			return MoveStatement();
+		// Swap statement
 		if (viewer.CheckType(SyntaxType.SwapKeyword))
 			return SwapStatement();
+		// Copy statement
 		if (viewer.CheckType(SyntaxType.CopyKeyword))
 			return CopyStatement();
+		// Assignment statement
 		if (viewer.MatchTypePattern(SyntaxType.Identifier, SyntaxType.EqualsToken))
 			return AssignmentStatement();
 
+		// Statement is an expression statement if not any of the above
 		return ExpressionStatement();
 	}
 	private AssignmentStatementSyntax AssignmentStatement() {
@@ -107,6 +114,8 @@ public sealed class Parser {
 	}
 	private ExpressionStatementSyntax ExpressionStatement() {
 		var expression = Expression();
+		// Only function invocations can be used as statements
+		// Made this prettier later
 		if (expression is not FunctionInvocationExpressionSyntax) {
 			Diagnostic diagnostic = new(DiagnosticLevel.Error, "Only function invocation expressions may be used as expression statements", expression.Span);
 			diagnostics.Add(diagnostic);
@@ -119,6 +128,7 @@ public sealed class Parser {
 	private IExpressionSyntax Range() {
 		var expression = Additive();
 
+		// Range expression
 		if (viewer.CheckType(SyntaxType.DotDotToken)) {
 			var dotDotToken = viewer.AdvanceCurrent();
 			var right = Additive();
@@ -130,11 +140,13 @@ public sealed class Parser {
 	private IExpressionSyntax Additive() {
 		var expression = Multiplicative();
 
+		// Binary addition expression
 		if (viewer.CheckType(SyntaxType.PlusToken)) {
 			var plusToken = viewer.AdvanceCurrent();
 			var right = Multiplicative();
 			return new BinaryAdditionExpressionSyntax(expression, plusToken, right);
 		}
+		// Binary subtraction expression
 		if (viewer.CheckType(SyntaxType.MinusToken)) {
 			var minusToken = viewer.AdvanceCurrent();
 			var right = Multiplicative();
@@ -146,21 +158,25 @@ public sealed class Parser {
 	private IExpressionSyntax Multiplicative() {
 		var expression = Unary();
 
+		// Binary multiplication expression
 		if (viewer.CheckType(SyntaxType.StarToken)) {
 			var starToken = viewer.AdvanceCurrent();
 			var right = Unary();
 			return new BinaryMultiplicationExpressionSyntax(expression, starToken, right);
 		}
+		// Binary division expression
 		if (viewer.CheckType(SyntaxType.SlashToken)) {
 			var slashToken = viewer.AdvanceCurrent();
 			var right = Unary();
 			return new BinaryDivisionExpressionSyntax(expression, slashToken, right);
 		}
+		// Binary modulo expression
 		if (viewer.CheckType(SyntaxType.PercentToken)) {
 			var percentToken = viewer.AdvanceCurrent();
 			var right = Unary();
 			return new BinaryModuloExpressionSyntax(expression, percentToken, right);
 		}
+		// Binary concatenation expression
 		if (viewer.CheckType(SyntaxType.LesserThanToken)) {
 			var lessThanToken = viewer.AdvanceCurrent();
 			var right = Unary();
@@ -173,11 +189,15 @@ public sealed class Parser {
 		if (viewer.CheckType(SyntaxType.PlusToken, SyntaxType.MinusToken, SyntaxType.StarToken)) {
 			var op = viewer.AdvanceCurrent();
 
+			// Unary addition expression
 			if (op.Type == SyntaxType.PlusToken)
 				return new UnaryPlusExpressionSyntax(op, Primary());
+			// Unary negation expression
 			if (op.Type == SyntaxType.MinusToken)
 				return new UnaryNegationExpressionSyntax(op, Primary());
+			// Some kind of function pointer expression
 			if (op.Type == SyntaxType.StarToken) {
+				// Unary line pointer expression
 				if (viewer.CheckType(SyntaxType.OpenSquareToken)) {
 					var openSquareToken = viewer.AdvanceCurrent();
 					var expression = Primary();
@@ -185,6 +205,7 @@ public sealed class Parser {
 					return new UnaryLinePointerExpressionSyntax(op, openSquareToken, expression, closeSquareToken);
 				}
 
+				// Unary line pointer expression
 				var identifier = ((VariableExpressionSyntax)Primary()).Identifier;
 				return new UnaryFunctionPointerExpressionSyntax(op, identifier);
 			}
@@ -193,16 +214,21 @@ public sealed class Parser {
 		return Primary();
 	}
 	private IExpressionSyntax Primary() {
+		// Here expression
 		if (viewer.CheckType(SyntaxType.HereKeyword))
 			return new HereExpressionSyntax(viewer.AdvanceCurrent());
+		// Start expression
 		if (viewer.CheckType(SyntaxType.StartKeyword))
 			return new StartExpressionSyntax(viewer.AdvanceCurrent());
+		// End expression
 		if (viewer.CheckType(SyntaxType.EndKeyword))
 			return new EndExpressionSyntax(viewer.AdvanceCurrent());
 
+		// Literal expression
 		if (viewer.CheckType(SyntaxType.NumberLiteral, SyntaxType.StringLiteral))
 			return new LiteralExpressionSyntax(viewer.AdvanceCurrent());
 
+		// Grouping expression
 		if (viewer.CheckType(SyntaxType.OpenBracketToken)) {
 			var openBracketToken = viewer.AdvanceCurrent();
 			var expression = Expression();
