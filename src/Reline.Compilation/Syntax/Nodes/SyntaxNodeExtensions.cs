0d07f25ecaa6eaf07909bd7c0d953024dad95800
thinker227 <thinker0227@gmail.com>
2022-01-03 16:46:19 +0100

    Added GetTextSpan extension

--- src/Reline.Compilation/Syntax/Nodes/SyntaxNodeExtensions.cs
+++ src/Reline.Compilation/Syntax/Nodes/SyntaxNodeExtensions.cs
@@ -33,4 +33,59 @@ public static class SyntaxNodeExtensions {
 		return node with { Diagnostics = newDiagnostics };
 	}
 
+	/// <summary>
+	/// Gets the <see cref="TextSpan"/> of a syntax node.
+	/// </summary>
+	/// <param name="node">The node to get the text span of.</param>
+	/// <returns>The <see cref="TextSpan"/> of <paramref name="node"/>.</returns>
+	/// <exception cref="NotSupportedException">The syntax node type is not supported.</exception>
+	public static TextSpan GetTextSpan(this ISyntaxNode node) =>
+		node switch {
+			ProgramSyntax program =>
+				new(GetTextSpan(program.Lines[0]).Start, GetTextSpan(program.Lines[^1]).End),
+			LineSyntax line => new(
+				line.Label?.GetTextSpan().Start ??
+				line.Statement?.GetTextSpan().Start ??
+				line.NewlineToken.Span.Start,
+				line.NewlineToken.Span.End),
+			LabelSyntax label => new(
+				label.Identifier.Name.Span.Start,
+				label.ColonToken.Span.End),
+			IdentifierSyntax identifier => identifier.Name.Span,
+
+			AssignmentStatementSyntax assignment => new(
+				assignment.Identifier.Name.Span.Start,
+				assignment.Initializer.GetTextSpan().End),
+			MoveStatementSyntax move => new(
+				move.MoveKeyword.Span.Start,
+				move.Target.GetTextSpan().End),
+			SwapStatementSyntax swap => new(
+				swap.SwapKeyword.Span.Start,
+				swap.Target.GetTextSpan().End),
+			CopyStatementSyntax copy => new(
+				copy.CopyKeyword.Span.Start,
+				copy.Target.GetTextSpan().End),
+			ExpressionStatementSyntax expression => expression.GetTextSpan(),
+
+			IBinaryExpressionSyntax binary => new(
+				binary.Left.GetTextSpan().Start,
+				binary.Right.GetTextSpan().End),
+			IUnaryExpressionSyntax unary => new(
+				unary.UnaryToken.Span.Start,
+				unary.Expression.GetTextSpan().End),
+			ITokenExpressionSyntax token => token.Token.Span,
+			GroupingExpressionSyntax grouping => new(
+				grouping.OpenBracketToken.Span.Start,
+				grouping.CloseBracketToken.Span.End),
+			FunctionInvocationExpressionSyntax invocation => new(
+				invocation.Identifier.Name.Span.Start,
+				invocation.CloseBracketToken.Span.End),
+			IdentifierExpressionSyntax identifier => identifier.Identifier.Name.Span,
+			UnaryLinePointerExpressionSyntax pointer => new(
+				pointer.StarToken.Span.Start,
+				pointer.CloseSquareToken.Span.End),
+
+			_ => throw new NotSupportedException($"Cannot retrieve text span of node type {node.GetType().Name}")
+		};
+
 }
