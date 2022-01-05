3402728393c939d099dc79cf5c9c2ed4e3329528
thinker227 <thinker0227@gmail.com>
2022-01-06 00:31:29 +0100

    Update SymbolExtensions.cs

--- src/Reline.Compilation/Symbols/SymbolExtensions.cs
+++ src/Reline.Compilation/Symbols/SymbolExtensions.cs
@@ -4,7 +4,4 @@ namespace Reline.Compilation.Symbols;
 
 public static class SymbolExtensions {
 
-	public static TNode GetSyntaxAs<TNode>(this ISymbol symbol) where TNode : ISyntaxNode =>
-		(TNode)symbol.Syntax;
-
 }
