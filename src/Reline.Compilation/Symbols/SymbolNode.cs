ea3638d54b17af1fc35982b2cd0d0cb02d11d2b5
thinker227 <thinker0227@gmail.com>
2022-01-07 15:55:47 +0100

    git messed up

--- src/Reline.Compilation/Symbols/SymbolNode.cs
+++ src/Reline.Compilation/Symbols/SymbolNode.cs
@@ -6,7 +6,6 @@ public abstract class SymbolNode : ISymbol {
 
 	public void Accept(IVisitor<ISymbol> visitor) =>
 		visitor.Visit(this);
-	public T Accept<T>(IVisitor<T> visitor) =>
 	public TResult Accept<TResult>(IVisitor<ISymbol, TResult> visitor) =>
 		visitor.Visit(this);
 
