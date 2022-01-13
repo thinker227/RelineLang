fbf67b396aa4d9c5865a19aeea9dc7e517cb33d6
thinker227 <thinker0227@gmail.com>
2022-01-13 20:19:44 +0100

    Added FunctionSymbol and ReturnStatementSymbol

---
+++ src/Reline.Compilation/Symbols/Declarations/FunctionSymbol.cs
@@ -0,0 +1,11 @@
+﻿namespace Reline.Compilation.Symbols.Declarations;
+
+public sealed class FunctionSymbol : SymbolNode {
+
+	public string Identifier { get; set; } = null!;
+	public IExpressionSymbol BodyExpression { get; set; } = null!;
+	public ICollection<LineSymbol> Body { get; } = new List<LineSymbol>();
+	public ITypeSymbol ReturnType { get; set; } = null!;
+	public ICollection<VariableSymbol> Parameters { get; } = new List<VariableSymbol>();
+
+}
---
+++ src/Reline.Compilation/Symbols/Statements/ReturnStatementSymbol.cs
@@ -0,0 +1,7 @@
+﻿namespace Reline.Compilation.Symbols.Statements;
+
+public sealed class ReturnStatementSymbol : SymbolNode, IStatementSymbol {
+
+	public IExpressionSymbol Expression { get; set; } = null!;
+
+}
