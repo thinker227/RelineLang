a4419e26c412ed221e5c4253e0dc5537e335a4ef
thinker227 <thinker0227@gmail.com>
2022-01-20 19:39:41 +0100

    Update Program.cs

--- src/Reline.CommandLine/Program.cs
+++ src/Reline.CommandLine/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Reline.Compilation;
 using Reline.Compilation.Lexing;
 using Reline.Compilation.Parsing;
+using Reline.Compilation.Binding;
 
 if (args.Length == 0) return;
 var path = args[0];
@@ -13,5 +14,7 @@ var lexResult = Lexer.LexSource(text);
 if (lexResult.HasErrors()) return;
 
 var parseResult = Parser.ParseTokens(lexResult);
+
+var bindResult = Binder.BindTree(parseResult.Result);
 
 Console.ReadLine();
