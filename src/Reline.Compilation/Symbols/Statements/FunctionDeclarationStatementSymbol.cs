89a4a5fe7dfc63d6c55b0d7ae2f95ffd6584aa2b
thinker227 <thinker0227@gmail.com>
2022-01-25 18:05:29 +0100

    Create FunctionDeclarationStatementSymbol.cs

---
+++ src/Reline.Compilation/Symbols/Statements/FunctionDeclarationStatementSymbol.cs
@@ -0,0 +1,7 @@
+﻿namespace Reline.Compilation.Symbols;
+
+public sealed class FunctionDeclarationStatementSymbol : SymbolNode, IStatementSymbol {
+
+	public FunctionSymbol Function { get; set; } = null!;
+
+}
