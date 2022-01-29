e211f803e0d7b2d212d6a8f7bc1723f4a2e4a412
thinker227 <thinker0227@gmail.com>
2022-01-30 00:20:00 +0100

    Made LiteralType to its own file and made it a flag enum

---
+++ src/Reline.Compilation/Symbols/LiteralType.cs
@@ -0,0 +1,24 @@
+﻿namespace Reline.Compilation.Symbols;
+
+/// <summary>
+/// Represents the type of a literal.
+/// </summary>
+[Flags]
+public enum LiteralType {
+	/// <summary>
+	/// The literal does not have a type or is invalid.
+	/// </summary>
+	None = 0,
+	/// <summary>
+	/// The literal is a number.
+	/// </summary>
+	Number = 1,
+	/// <summary>
+	/// The literal is a string.
+	/// </summary>
+	String = 2,
+	/// <summary>
+	/// The literal is a range.
+	/// </summary>
+	Range = 4,
+}
--- src/Reline.Compilation/Symbols/LiteralValue.cs
+++ src/Reline.Compilation/Symbols/LiteralValue.cs
@@ -131,25 +131,3 @@ public readonly struct LiteralValue : IEquatable<LiteralValue> {
 		new(range);
 
 }
-
-/// <summary>
-/// Represents the type of a literal.
-/// </summary>
-public enum LiteralType {
-	/// <summary>
-	/// The literal does not have a type or is invalid.
-	/// </summary>
-	None,
-	/// <summary>
-	/// The literal is a number.
-	/// </summary>
-	Number,
-	/// <summary>
-	/// The literal is a string.
-	/// </summary>
-	String,
-	/// <summary>
-	/// The literal is a range.
-	/// </summary>
-	Range
-}
