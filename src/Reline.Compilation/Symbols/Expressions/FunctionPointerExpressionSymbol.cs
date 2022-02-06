a1134dc9420d675acab73249350ab1384b8010fd
thinker227 <thinker0227@gmail.com>
2022-02-06 19:22:50 +0100

    Update FunctionPointerExpressionSymbol.cs

--- src/Reline.Compilation/Symbols/Expressions/FunctionPointerExpressionSymbol.cs
+++ src/Reline.Compilation/Symbols/Expressions/FunctionPointerExpressionSymbol.cs
@@ -3,6 +3,7 @@
 public sealed class FunctionPointerExpressionSymbol : SymbolNode, IExpressionSymbol {
 
 	public FunctionSymbol Function { get; set; } = null!;
+	public bool IsConstant => true;
 
 	public T Accept<T>(IExpressionVisitor<T> visitor) => visitor.VisitFunctionPointer(this);
 
