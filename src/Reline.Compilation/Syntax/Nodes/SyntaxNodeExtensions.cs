e07b0a6fc15423f2717492c6906eeb67d438b2d0
thinker227 <thinker0227@gmail.com>
2022-02-06 18:35:06 +0100

    Update SyntaxNodeExtensions.cs

--- src/Reline.Compilation/Syntax/Nodes/SyntaxNodeExtensions.cs
+++ src/Reline.Compilation/Syntax/Nodes/SyntaxNodeExtensions.cs
@@ -1,6 +1,4 @@
-﻿using Reline.Compilation.Diagnostics;
-
-namespace Reline.Compilation.Syntax.Nodes;
+﻿namespace Reline.Compilation.Syntax.Nodes;
 
 public static class SyntaxNodeExtensions {
 
@@ -11,55 +9,7 @@ public static class SyntaxNodeExtensions {
 	/// <returns>The <see cref="TextSpan"/> of <paramref name="node"/>.</returns>
 	/// <exception cref="NotSupportedException">The syntax node type is not supported.</exception>
 	public static TextSpan GetTextSpan(this ISyntaxNode node) =>
-		node switch {
-			ProgramSyntax program => new(
-				program.Lines[0].GetTextSpan().Start,
-				program.Lines[^1].GetTextSpan().End),
-			LineSyntax line => new(
-				line.Label?.GetTextSpan().Start ??
-				line.Statement?.GetTextSpan().Start ??
-				line.NewlineToken.Span.Start,
-				line.NewlineToken.Span.End),
-			LabelSyntax label => new(
-				label.Identifier.Span.Start,
-				label.ColonToken.Span.End),
-			ParameterListSyntax pList => new(
-				pList.OpenBracketToken.Span.Start,
-				pList.CloseBracketToken.Span.End),
-
-			ExpressionStatementSyntax expression => expression.GetTextSpan(),
-			AssignmentStatementSyntax assignment => new(
-				assignment.Identifier.Span.Start,
-				assignment.Initializer.GetTextSpan().End),
-			IManipulationStatementSyntax manipulation => new(
-				manipulation.SourceKeyword.Span.Start,
-				manipulation.Target.GetTextSpan().End),
-			ReturnStatementSyntax @return => new(
-				@return.ReturnKeyword.Span.Start,
-				@return.Expression.GetTextSpan().End),
-			FunctionDeclarationStatementSyntax function => new(
-				function.FunctionKeyword.Span.Start,
-				function.ParameterList?.GetTextSpan().End ??
-				function.Body.GetTextSpan().End),
-
-			UnaryExpressionSyntax unary => new(
-				unary.OperatorToken.Span.Start,
-				unary.Expression.GetTextSpan().End),
-			BinaryExpressionSyntax binary => new(
-				binary.Left.GetTextSpan().Start,
-				binary.Right.GetTextSpan().End),
-			KeywordExpressionSyntax keyword => keyword.Keyword.Span,
-			LiteralExpressionSyntax literal => literal.Literal.Span,
-			GroupingExpressionSyntax grouping => new(
-				grouping.OpenBracketToken.Span.Start,
-				grouping.CloseBracketToken.Span.End),
-			FunctionInvocationExpressionSyntax invocation => new(
-				invocation.Identifier.Span.Start,
-				invocation.CloseBracketToken.Span.End),
-			IdentifierExpressionSyntax identifier => identifier.Identifier.Span,
-
-			_ => throw new NotSupportedException($"Cannot retrieve text span of node type {node.GetType().Name}")
-		};
+		node.Accept(TextSpanEvaluator.Instance);
 
 	/// <summary>
 	/// Gets the child nodes of a syntax node.
@@ -68,45 +18,7 @@ public static class SyntaxNodeExtensions {
 	/// <returns>The child nodes of <paramref name="node"/>.</returns>
 	/// <exception cref="NotSupportedException">The syntax node type is not supported.</exception>
 	public static ImmutableArray<ISyntaxNode> GetChildren(this ISyntaxNode node) =>
-		node switch {
-			ProgramSyntax program =>
-				program.Lines.As<ISyntaxNode>(),
-			LineSyntax line =>
-				ImmutableArray<ISyntaxNode>.Empty
-				.AddNotNull(line.Label)
-				.AddNotNull(line.Statement),
-			ParameterListSyntax pList =>
-				pList.Parameters.As<ISyntaxNode>(),
-
-			ExpressionStatementSyntax expression =>
-				ImmutableArray.Create<ISyntaxNode>(expression.Expression),
-			AssignmentStatementSyntax assignment =>
-				ImmutableArray.Create<ISyntaxNode>(assignment.Initializer),
-			IManipulationStatementSyntax manipulation =>
-				ImmutableArray.Create<ISyntaxNode>(manipulation.Source, manipulation.Target),
-			ReturnStatementSyntax @return =>
-				ImmutableArray.Create<ISyntaxNode>(@return.Expression),
-			FunctionDeclarationStatementSyntax function =>
-				ImmutableArray.Create<ISyntaxNode>(function.Body)
-				.AddNotNull(function.ParameterList),
-
-			UnaryExpressionSyntax unary =>
-				ImmutableArray.Create<ISyntaxNode>(unary.Expression),
-			BinaryExpressionSyntax binary =>
-				ImmutableArray.Create<ISyntaxNode>(binary.Left, binary.Right),
-			GroupingExpressionSyntax grouping =>
-				ImmutableArray.Create<ISyntaxNode>(grouping.Expression),
-			FunctionInvocationExpressionSyntax invocation =>
-				invocation.Arguments.As<ISyntaxNode>(),
-			IdentifierExpressionSyntax identifier =>
-				ImmutableArray.Create<ISyntaxNode>(identifier),
-
-			LabelSyntax or
-			LiteralExpressionSyntax  =>
-				ImmutableArray<ISyntaxNode>.Empty,
-
-			_ => throw new NotSupportedException($"Cannot retrieve children of node type {node.GetType().Name}")
-		};
+		node.Accept(ChildEvaluator.Instance);
 	/// <summary>
 	/// Recursively gets all the child nodes of a syntax node.
 	/// </summary>
@@ -126,5 +38,121 @@ public static class SyntaxNodeExtensions {
 	/// of type <typeparamref name="TNode"/>.</returns>
 	public static ImmutableArray<TNode> GetAllDescendants<TNode>(this ISyntaxNode node) where TNode : ISyntaxNode =>
 		GetAllDescendants(node).OfType<TNode>().ToImmutableArray();
+
+	private sealed class TextSpanEvaluator : ISyntaxVisitor<TextSpan> {
+
+		public static TextSpanEvaluator Instance { get; } = new();
+		private TextSpanEvaluator() { }
+
+		public TextSpan VisitProgram(ProgramSyntax syntax) => new(
+			syntax.Lines[0].GetTextSpan().Start,
+			syntax.Lines[^1].GetTextSpan().End);
+		public TextSpan VisitLine(LineSyntax syntax) => new(
+			syntax.Label?.GetTextSpan().Start ??
+			syntax.Statement?.GetTextSpan().Start ??
+			syntax.NewlineToken.Span.Start,
+			syntax.NewlineToken.Span.End);
+		public TextSpan VisitLabel(LabelSyntax syntax) => new(
+			syntax.Identifier.Span.Start,
+			syntax.ColonToken.Span.End);
+		public TextSpan VisitParameterList(ParameterListSyntax syntax) => new(
+			syntax.OpenBracketToken.Span.Start,
+			syntax.CloseBracketToken.Span.End);
+
+		public TextSpan VisitAssignmentStatement(AssignmentStatementSyntax syntax) => new(
+			syntax.Identifier.Span.Start,
+			syntax.Initializer.GetTextSpan().End);
+		public TextSpan VisitExpressionStatement(ExpressionStatementSyntax syntax) =>
+			syntax.GetTextSpan();
+		public TextSpan VisitMoveStatement(MoveStatementSyntax syntax) => new(
+			syntax.MoveKeyword.Span.Start,
+			syntax.Target.GetTextSpan().End);
+		public TextSpan VisitSwapStatement(SwapStatementSyntax syntax) => new(
+			syntax.SwapKeyword.Span.Start,
+			syntax.Target.GetTextSpan().End);
+		public TextSpan VisitCopyStatement(CopyStatementSyntax syntax) => new(
+			syntax.CopyKeyword.Span.Start,
+			syntax.Target.GetTextSpan().End);
+		public TextSpan VisitFunctionDeclarationStatement(FunctionDeclarationStatementSyntax syntax) => new(
+			syntax.FunctionKeyword.Span.Start,
+			syntax.ParameterList?.GetTextSpan().End ??
+			syntax.Body.GetTextSpan().End);
+		public TextSpan VisitReturnStatement(ReturnStatementSyntax syntax) => new(
+			syntax.ReturnKeyword.Span.Start,
+			syntax.Expression.GetTextSpan().End);
+
+		public TextSpan VisitUnaryExpression(UnaryExpressionSyntax syntax) => new(
+			syntax.OperatorToken.Span.Start,
+			syntax.Expression.GetTextSpan().End);
+		public TextSpan VisitBinaryExpression(BinaryExpressionSyntax syntax) => new(
+			syntax.Left.GetTextSpan().Start,
+			syntax.Right.GetTextSpan().End);
+		public TextSpan VisitKeywordExpression(KeywordExpressionSyntax syntax) =>
+			syntax.Keyword.Span;
+		public TextSpan VisitLiteralExpression(LiteralExpressionSyntax syntax) =>
+			syntax.Literal.Span;
+		public TextSpan VisitGroupingExpression(GroupingExpressionSyntax syntax) => new(
+			syntax.OpenBracketToken.Span.Start,
+			syntax.CloseBracketToken.Span.End);
+		public TextSpan VisitIdentifierExpression(IdentifierExpressionSyntax syntax) =>
+			syntax.Identifier.Span;
+		public TextSpan VisitFunctionInvocationExpression(FunctionInvocationExpressionSyntax syntax) => new(
+			syntax.Identifier.Span.Start,
+			syntax.CloseBracketToken.Span.End);
+		public TextSpan VisitFunctionPointerExpression(FunctionPointerExpressionSyntax syntax) => new(
+			syntax.StarToken.Span.Start,
+			syntax.Identifier.Span.End);
+
+	}
+	private sealed class ChildEvaluator : ISyntaxVisitor<ImmutableArray<ISyntaxNode>> {
+
+		public static ChildEvaluator Instance { get; } = new();
+		private ChildEvaluator() { }
+
+		public ImmutableArray<ISyntaxNode> VisitProgram(ProgramSyntax syntax) =>
+			syntax.Lines.As<ISyntaxNode>();
+		public ImmutableArray<ISyntaxNode> VisitLine(LineSyntax syntax) =>
+			ImmutableArray<ISyntaxNode>.Empty
+				.AddNotNull(syntax.Label)
+				.AddNotNull(syntax.Statement);
+		public ImmutableArray<ISyntaxNode> VisitLabel(LabelSyntax syntax) =>
+			ImmutableArray<ISyntaxNode>.Empty;
+		public ImmutableArray<ISyntaxNode> VisitParameterList(ParameterListSyntax syntax) =>
+			syntax.Parameters.As<ISyntaxNode>();
+
+		public ImmutableArray<ISyntaxNode> VisitAssignmentStatement(AssignmentStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Initializer);
+		public ImmutableArray<ISyntaxNode> VisitExpressionStatement(ExpressionStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Expression);
+		public ImmutableArray<ISyntaxNode> VisitMoveStatement(MoveStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Source, syntax.Target);
+		public ImmutableArray<ISyntaxNode> VisitSwapStatement(SwapStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Source, syntax.Target);
+		public ImmutableArray<ISyntaxNode> VisitCopyStatement(CopyStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Source, syntax.Target);
+		public ImmutableArray<ISyntaxNode> VisitFunctionDeclarationStatement(FunctionDeclarationStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Body)
+			.AddNotNull(syntax.ParameterList);
+		public ImmutableArray<ISyntaxNode> VisitReturnStatement(ReturnStatementSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Expression);
+
+		public ImmutableArray<ISyntaxNode> VisitUnaryExpression(UnaryExpressionSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Expression);
+		public ImmutableArray<ISyntaxNode> VisitBinaryExpression(BinaryExpressionSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Left, syntax.Right);
+		public ImmutableArray<ISyntaxNode> VisitKeywordExpression(KeywordExpressionSyntax syntax) =>
+			ImmutableArray<ISyntaxNode>.Empty;
+		public ImmutableArray<ISyntaxNode> VisitLiteralExpression(LiteralExpressionSyntax syntax) =>
+			ImmutableArray<ISyntaxNode>.Empty;
+		public ImmutableArray<ISyntaxNode> VisitGroupingExpression(GroupingExpressionSyntax syntax) =>
+			ImmutableArray.Create<ISyntaxNode>(syntax.Expression);
+		public ImmutableArray<ISyntaxNode> VisitIdentifierExpression(IdentifierExpressionSyntax syntax) =>
+			ImmutableArray<ISyntaxNode>.Empty;
+		public ImmutableArray<ISyntaxNode> VisitFunctionInvocationExpression(FunctionInvocationExpressionSyntax syntax) =>
+			syntax.Arguments.As<ISyntaxNode>();
+		public ImmutableArray<ISyntaxNode> VisitFunctionPointerExpression(FunctionPointerExpressionSyntax syntax) =>
+			ImmutableArray<ISyntaxNode>.Empty;
+
+	}
 
 }
