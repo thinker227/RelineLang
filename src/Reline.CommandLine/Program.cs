eee5669c0015d9bbcf87f4eed5f523987a92d2ef
thinker227 <thinker0227@gmail.com>
2022-02-12 22:25:46 +0100

    Update Program.cs

--- src/Reline.CommandLine/Program.cs
+++ src/Reline.CommandLine/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.IO;
-using Reline.Compilation;
-using Reline.Compilation.Lexing;
 using Reline.Compilation.Parsing;
 using Reline.Compilation.Binding;
 
 if (args.Length == 0) return;
-var path = args[0];
+string path = args[0];
 if (!File.Exists(path)) return;
-var text = File.ReadAllText(path);
+string text = File.ReadAllText(path);
 
 var parseResult = Parser.ParseString(text);
 
