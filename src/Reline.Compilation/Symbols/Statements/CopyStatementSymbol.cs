1afac8ba15dc7b17266f026d0a2f9620e3541202
thinker227 <thinker0227@gmail.com>
2022-02-28 00:51:53 +0100

    Added ManipulationStatementSymbol base

--- src/Reline.Compilation/Symbols/Statements/CopyStatementSymbol.cs
+++ src/Reline.Compilation/Symbols/Statements/CopyStatementSymbol.cs
@@ -3,15 +3,6 @@
 /// <summary>
 /// Represents a <c>copy</c> statement.
 /// </summary>
-public sealed class CopyStatementSymbol : SymbolNode, IStatementSymbol {
-
-	/// <summary>
-	/// The source range.
-	/// </summary>
-	public IExpressionSymbol Source { get; set; } = null!;
-	/// <summary>
-	/// The target line.
-	/// </summary>
-	public IExpressionSymbol Target { get; set; } = null!;
+public sealed class CopyStatementSymbol : ManipulationStatementSymbol {
 
 }
