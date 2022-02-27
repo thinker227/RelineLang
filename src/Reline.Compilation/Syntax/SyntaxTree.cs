ffb4e1bfe0398e114a2d3c8eb896b5ce275d55f5
thinker227 <thinker0227@gmail.com>
2022-02-27 19:00:30 +0100

    Added GetParent method
    Praise Immo Landwerth. https://github.com/terrajobst/minsk/blob/master/docs/episode-27.md

--- src/Reline.Compilation/Syntax/SyntaxTree.cs
+++ src/Reline.Compilation/Syntax/SyntaxTree.cs
@@ -1,18 +1,47 @@
-﻿using Reline.Compilation.Syntax.Nodes;
+﻿using System.Threading;
+using Reline.Compilation.Syntax.Nodes;
 using Reline.Compilation.Diagnostics;
 
 namespace Reline.Compilation.Syntax;
 
 public sealed class SyntaxTree {
 
+	private IReadOnlyDictionary<ISyntaxNode, ISyntaxNode?>? parents;
+
+
+	
 	public ImmutableArray<Diagnostic> Diagnostics { get; }
 	public ProgramSyntax Root { get; }
 
 
 
-	private SyntaxTree(ProgramSyntax root, ImmutableArray<Diagnostic> diagnostics) {
+	internal SyntaxTree(ProgramSyntax root, ImmutableArray<Diagnostic> diagnostics) {
 		Root = root;
 		Diagnostics = diagnostics;
+	}
+
+
+
+	public ISyntaxNode? GetParent(ISyntaxNode node) {
+		if (parents is null) {
+			var initializedParents = CreateParentsDictionary(Root);
+			Interlocked.CompareExchange(ref parents, initializedParents, null);
+		}
+		
+		return parents[node];
+	}
+	private static IReadOnlyDictionary<ISyntaxNode, ISyntaxNode?> CreateParentsDictionary(ISyntaxNode root) {
+		Dictionary<ISyntaxNode, ISyntaxNode?> result = new();
+		result.Add(root, null);
+		AddParentsToDictionary(result, root);
+		return result;
+	}
+	private static void AddParentsToDictionary(IDictionary<ISyntaxNode, ISyntaxNode?> dictionary, ISyntaxNode node) {
+		var children = node.GetChildren();
+		foreach (var child in children) {
+			dictionary.Add(child, node);
+			AddParentsToDictionary(dictionary, child);
+		}
 	}
 
 }
