23d0e298389e07fecc022aec5f9b58a9ec1ded12
thinker227 <thinker0227@gmail.com>
2022-03-01 21:49:01 +0100

    Added documentation to ISyntaxNode

--- src/Reline.Compilation/Syntax/Nodes/ISyntaxNode.cs
+++ src/Reline.Compilation/Syntax/Nodes/ISyntaxNode.cs
@@ -5,7 +5,18 @@
 /// </summary>
 public interface ISyntaxNode : INode<ISyntaxNode> {
 
+	/// <summary>
+	/// Accepts a <see cref="ISyntaxVisitor{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The return type of the visitor.</typeparam>
+	/// <param name="visitor">The <see cref="ISyntaxVisitor{T}"/> to accept.</param>
+	/// <returns>The return value of <paramref name="visitor"/>.</returns>
 	T Accept<T>(ISyntaxVisitor<T> visitor);
+	/// <summary>
+	/// The the span of the syntax node in the source text.
+	/// </summary>
+	/// <returns>A <see cref="TextSpan"/> representing the span
+	/// of the syntax node in the source text.</returns>
 	TextSpan GetTextSpan();
 
 }
