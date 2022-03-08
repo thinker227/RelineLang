358b689709ee016781a91891e294318fb8d0a3b9
thinker227 <thinker0227@gmail.com>
2022-03-08 22:03:40 +0100

    Update NativeFunction.cs

--- src/Reline.Compilation/Symbols/NativeFunction.cs
+++ src/Reline.Compilation/Symbols/NativeFunction.cs
@@ -4,25 +4,25 @@ namespace Reline.Compilation;
 /// The opcode of a native function.
 /// </summary>
 public enum NativeFunction {
-    /// <summary>
-    /// The function is unknown or could not be found.
-    /// </summary>
-    Unknown = 0,
+	/// <summary>
+	/// The function is unknown or could not be found.
+	/// </summary>
+	Unknown = 0,
 
-    // IO (001 to 099)
-    Write = 001,
-    ReadLine = 002,
+	// IO (001 to 099)
+	Write = 001,
+	ReadLine = 002,
 
-    // Conversions (101 to 199)
-    String = 101,
-    ParseInt = 102,
+	// Conversions (101 to 199)
+	String = 101,
+	ParseInt = 102,
 
-    // Math (201 to 299)
-    Clamp = 201,
-    Min = 202,
-    Max = 203,
+	// Math (201 to 299)
+	Clamp = 201,
+	Min = 202,
+	Max = 203,
 
-    // String operations (301 to 399)
-    StringIndex = 301,
-    Ascii = 302,
+	// String operations (301 to 399)
+	StringIndex = 301,
+	Ascii = 302,
 }
