9a7f5414f37f7d5abdb63f22ab53f155f522cefe
thinker227 <thinker0227@gmail.com>
2022-03-26 17:44:54 +0100

    Moved pack/install/update scripts

--- src/Reline.CommandLine/pack-and-update.ps1
+++ src/Reline.CommandLine/pack-and-update.ps1
@@ -1,4 +1,3 @@
 # Script to pack and update the current version of the compiler CLI
-Set-Location ./Reline.CommandLine
 dotnet pack --no-restore
 dotnet tool update --global --version 1.0.0-dev --add-source ./nupkg Reline.CommandLine
