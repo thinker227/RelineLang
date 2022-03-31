7ef853d2291bc03f69b36188c386776824be7b79
thinker227 <thinker0227@gmail.com>
2022-03-31 22:29:03 +0200

    Added TreeTestBase

--- src/Reline.Tests/ParserTestBase.cs
+++ src/Reline.Tests/ParserTestBase.cs
@@ -2,15 +2,12 @@
 using Reline.Common;
 using Reline.Compilation.Parsing;
 using Reline.Compilation.Syntax.Nodes;
-using Xunit.Sdk;
 
 namespace Reline.Tests;
 
-public abstract class ParserTestBase : TestBase {
+public abstract class ParserTestBase : TreeTestBase<ISyntaxNode> {
 
-	private IEnumerator<ISyntaxNode>? nodeEnumerator;
-
-
+	private IEnumerable<ISyntaxNode>? enumerable;
 
 	/// <summary>
 	/// Sets the tree to traverse from a source string.
@@ -18,52 +15,15 @@ public abstract class ParserTestBase : TestBase {
 	/// <param name="source">The source stirng to generate the tree from.</param>
 	protected void SetTree(string source) {
 		var tree = AssertAsync.CompletesIn(2000, () => Parser.ParseString(source));
-		var nodes = tree.Root.GetAllDescendants();
-		nodeEnumerator = nodes.AsEnumerable()
+		var nodes = tree.Root.GetDescendants();
+		enumerable = nodes
 			.Prepend(tree.Root) // Test for ProgramSyntax
-			.GetEnumerator();
+			.Select(n => (ISyntaxNode)n);
 	}
 
-	/// <summary>
-	/// Asserts that the current node is of a specified type.
-	/// </summary>
-	/// <typeparam name="TNode">The type of the node to assert.</typeparam>
-	/// <returns>The current node as <typeparamref name="TNode"/>.</returns>
-	/// <exception cref="InvalidOperationException">
-	/// <see cref="SetTree(string)"/> has not been called
-	/// and the tree to enumerate is <see langword="null"/>.
-	/// </exception>
-	protected TNode Node<TNode>() where TNode : ISyntaxNode {
-		if (nodeEnumerator is null)
-			throw new InvalidOperationException($"{nameof(SetTree)} must be called before {nameof(Node)}.");
-
-		try {
-			Assert.True(nodeEnumerator.MoveNext(), "The tree contained no more nodes.");
-			var current = nodeEnumerator.Current;
-			Assert.IsType<TNode>(current);
-			return (TNode)current;
-		} catch (XunitException e) {
-			nodeEnumerator = null;
-			throw e;
-		}
-	}
-
-	/// <summary>
-	/// Asserts that there are no more nodes in the tree.
-	/// </summary>
-	/// <exception cref="InvalidOperationException">
-	/// <see cref="SetTree(string)"/> has not been called
-	/// and the tree to enumerate is <see langword="null"/>.
-	/// </exception>
-	protected void End() {
-		if (nodeEnumerator is null)
-			throw new InvalidOperationException($"{nameof(SetTree)} must be called before {nameof(End)}.");
-
-		try {
-			Assert.False(nodeEnumerator.MoveNext(), "The tree contained more nodes.");
-		} finally {
-			nodeEnumerator = null;
-		}
-	}
+	protected override IEnumerable<ISyntaxNode>? GetEnumerable() =>
+		enumerable;
+	protected override void Reset() =>
+		enumerable = null;
 
 }
---
+++ src/Reline.Tests/TreeTestBase.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using Reline.Common;
+using Reline.Compilation.Parsing;
+using Reline.Compilation.Syntax.Nodes;
+using Xunit.Sdk;
+
+namespace Reline.Tests;
+
+/// <summary>
+/// Base for testing trees.
+/// </summary>
+/// <typeparam name="TNode">The type of node the class tests.</typeparam>
+public abstract class TreeTestBase<TNode> : TestBase where TNode : INode<TNode> {
+
+	private IEnumerator<TNode>? nodeEnumerator;
+
+
+
+	/// <summary>
+	/// Gets an enumerable to enumerate over the tree.
+	/// </summary>
+	/// <returns>An <see cref="IEnumerable{T}"/> of the nodes in the tree.</returns>
+	protected abstract IEnumerable<TNode>? GetEnumerable();
+	/// <summary>
+	/// Resets the state of the tree.
+	/// </summary>
+	protected abstract void Reset();
+
+	/// <summary>
+	/// Asserts that the current node is of a specified type.
+	/// </summary>
+	/// <typeparam name="T">The type of the node to assert.</typeparam>
+	/// <returns>The current node as <typeparamref name="T"/>.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// <see cref="SetTree(string)"/> has not been called
+	/// and the tree to enumerate is <see langword="null"/>.
+	/// </exception>
+	protected T Node<T>() where T : TNode {
+		if (nodeEnumerator is null) {
+			nodeEnumerator =
+				(GetEnumerable() ?? throw new InvalidOperationException("No tree to enumerate."))
+				.GetEnumerator();
+		}
+
+		try {
+			Assert.True(nodeEnumerator.MoveNext(), "The tree contained no more nodes.");
+			var current = nodeEnumerator.Current;
+			Assert.NotNull(current);
+			Assert.IsType<T>(current);
+			return (T)current!;
+		} catch (XunitException e) {
+			nodeEnumerator = null;
+			throw e;
+		}
+	}
+	/// <summary>
+	/// Asserts that there are no more nodes in the tree.
+	/// </summary>
+	/// <exception cref="InvalidOperationException">
+	/// <see cref="SetTree(string)"/> has not been called
+	/// and the tree to enumerate is <see langword="null"/>.
+	/// </exception>
+	protected void End() {
+		if (nodeEnumerator is null)
+			throw new InvalidOperationException($"No tree to enumerate.");
+
+		try {
+			Assert.False(nodeEnumerator.MoveNext(), "The tree contained more nodes.");
+		} finally {
+			nodeEnumerator = null;
+			Reset();
+		}
+	}
+
+}
