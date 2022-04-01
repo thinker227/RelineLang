b2fe0e182504a838a86702538f798ea7320e5809
thinker227 <thinker0227@gmail.com>
2022-04-02 00:41:19 +0200

    Added TokenSequenceBuilder

--- src/Reline.Tests/Tests/LexerTests.cs
+++ src/Reline.Tests/Tests/LexerTests.cs
@@ -8,22 +8,21 @@ public class LexerTests : TestBase {
 
 	[Fact]
 	public void LexesRegularTokens() {
-		var expectedTokens = new[] {
-			Token(SyntaxType.ColonToken, 0, ":"),
-			Token(SyntaxType.EqualsToken, 1, "="),
-			Token(SyntaxType.LesserThanToken, 2, "<"),
-			Token(SyntaxType.PlusToken, 3, "+"),
-			Token(SyntaxType.MinusToken, 4, "-"),
-			Token(SyntaxType.StarToken, 5, "*"),
-			Token(SyntaxType.SlashToken, 6, "/"),
-			Token(SyntaxType.BackslashToken, 7, "\\"),
-			Token(SyntaxType.PercentToken, 8, "%"),
-			Token(SyntaxType.OpenBracketToken, 9, "("),
-			Token(SyntaxType.CloseBracketToken, 10, ")"),
-			Token(SyntaxType.NewlineToken, 11, "\n"),
-			Token(SyntaxType.DotDotToken, 12, 13, ".."),
-			Token(SyntaxType.EndOfFile, TextSpan.Empty, ""),
-		};
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Add(SyntaxType.ColonToken, ":")
+			.Add(SyntaxType.EqualsToken, "=")
+			.Add(SyntaxType.LesserThanToken, "<")
+			.Add(SyntaxType.PlusToken, "+")
+			.Add(SyntaxType.MinusToken, "-")
+			.Add(SyntaxType.StarToken, "*")
+			.Add(SyntaxType.SlashToken, "/")
+			.Add(SyntaxType.BackslashToken, "\\")
+			.Add(SyntaxType.PercentToken, "%")
+			.Add(SyntaxType.OpenBracketToken, "(")
+			.Add(SyntaxType.CloseBracketToken, ")")
+			.Add(SyntaxType.NewlineToken, "\n")
+			.Add(SyntaxType.DotDotToken, "..")
+			.EOF();
 
 		string source =
 			":=<+-*/\\%()\n..";
@@ -34,19 +33,27 @@ public class LexerTests : TestBase {
 	}
 	[Fact]
 	public void LexesKeywordTokens() {
-		var expectedTokens = new[] {
-			Token(SyntaxType.HereKeyword, 0, 3, "here"),
-			Token(SyntaxType.StartKeyword, 5, 9, "start"),
-			Token(SyntaxType.EndKeyword, 11, 13, "end"),
-			Token(SyntaxType.MoveKeyword, 15, 18, "move"),
-			Token(SyntaxType.SwapKeyword, 20, 23, "swap"),
-			Token(SyntaxType.CopyKeyword, 25, 28, "copy"),
-			Token(SyntaxType.ToKeyword, 30, 31, "to"),
-			Token(SyntaxType.WithKeyword, 33, 36, "with"),
-			Token(SyntaxType.ReturnKeyword, 38, 43, "return"),
-			Token(SyntaxType.FunctionKeyword, 45, 52, "function"),
-			Token(SyntaxType.EndOfFile, TextSpan.Empty, ""),
-		};
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Add(SyntaxType.HereKeyword, "here")
+			.Trivia(" ")
+			.Add(SyntaxType.StartKeyword, "start")
+			.Trivia(" ")
+			.Add(SyntaxType.EndKeyword, "end")
+			.Trivia(" ")
+			.Add(SyntaxType.MoveKeyword, "move")
+			.Trivia(" ")
+			.Add(SyntaxType.SwapKeyword, "swap")
+			.Trivia(" ")
+			.Add(SyntaxType.CopyKeyword, "copy")
+			.Trivia(" ")
+			.Add(SyntaxType.ToKeyword, "to")
+			.Trivia(" ")
+			.Add(SyntaxType.WithKeyword, "with")
+			.Trivia(" ")
+			.Add(SyntaxType.ReturnKeyword, "return")
+			.Trivia(" ")
+			.Add(SyntaxType.FunctionKeyword, "function")
+			.EOF();
 
 		string source =
 			"here start end move swap copy to with return function";
@@ -57,10 +64,9 @@ public class LexerTests : TestBase {
 	}
 	[Fact]
 	public void LexesStrings() {
-		var expectedTokens = new[] {
-			Token(SyntaxType.StringLiteral, 0, 17, @"""this is a string""", "this is a string"),
-			Token(SyntaxType.EndOfFile, TextSpan.Empty, ""),
-		};
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Str("this is a string")
+			.EOF();
 
 		string source =
 			@"""this is a string""";
@@ -71,15 +77,19 @@ public class LexerTests : TestBase {
 	}
 	[Fact]
 	public void LexesNumbers() {
-		var expectedTokens = new[] {
-			Token(SyntaxType.NumberLiteral, 0, "0", 0),
-			Token(SyntaxType.NumberLiteral, 2, "7", 7),
-			Token(SyntaxType.NumberLiteral, 4, 8, "00123", 123),
-			Token(SyntaxType.NumberLiteral, 10, 18, "123456789", 123456789),
-			Token(SyntaxType.NumberLiteral, 20, 27, "10000000", 10000000),
-			Token(SyntaxType.NumberLiteral, 29, 36, "00000001", 1),
-			Token(SyntaxType.EndOfFile, TextSpan.Empty, ""),
-		};
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Num(0)
+			.Trivia(" ")
+			.Num(7)
+			.Trivia(" ")
+			.Num("00123", 123)
+			.Trivia(" ")
+			.Num(123456789)
+			.Trivia(" ")
+			.Num(10000000)
+			.Trivia(" ")
+			.Num("00000001", 1)
+			.EOF();
 
 		string source =
 			"0 7 00123 123456789 10000000 00000001";
@@ -90,17 +100,23 @@ public class LexerTests : TestBase {
 	}
 	[Fact]
 	public void LexesIdentifiers() {
-		var expectedTokens = new[] {
-			Token(SyntaxType.Identifier, 0, 2, "foo"),
-			Token(SyntaxType.Identifier, 4, 6, "bar"),
-			Token(SyntaxType.Identifier, 8, 10, "Baz"),
-			Token(SyntaxType.Identifier, 12, 40, "excessivelyLongIdentifierName"),
-			Token(SyntaxType.Identifier, 42, 54, "_a_b_c_d_e_f_"),
-			Token(SyntaxType.Identifier, 56, 57, "@i"),
-			Token(SyntaxType.Identifier, 59, 68, "a123456789"),
-			Token(SyntaxType.Identifier, 70, 72, "b1c"),
-			Token(SyntaxType.EndOfFile, TextSpan.Empty, ""),
-		};
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Add(SyntaxType.Identifier, "foo")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "bar")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "Baz")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "excessivelyLongIdentifierName")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "_a_b_c_d_e_f_")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "@i")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "a123456789")
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "b1c")
+			.EOF();
 
 		string source =
 			"foo bar Baz excessivelyLongIdentifierName _a_b_c_d_e_f_ @i a123456789 b1c";
@@ -115,17 +131,15 @@ public class LexerTests : TestBase {
 			"&$.[]{}";
 		var lexResult = AssertAsync.CompletesIn(1000, () => Lexer.LexSource(source));
 
-		var expectedTokens = new[] {
-			Token(SyntaxType.EndOfFile, TextSpan.Empty, "").WithLeadingTrivia(new[] {
-				Trivia(0, "&"),
-				Trivia(1, "$"),
-				Trivia(2, "."),
-				Trivia(3, "["),
-				Trivia(4, "]"),
-				Trivia(5, "{"),
-				Trivia(6, "}"),
-			}),
-		};
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Trivia("&")
+			.Trivia("$")
+			.Trivia(".")
+			.Trivia("[")
+			.Trivia("]")
+			.Trivia("{")
+			.Trivia("}")
+			.EOF();
 		SyntaxTokenEqualityComparer tokenComparer = new(0);
 		Assert.Equal(expectedTokens, lexResult.Tokens, tokenComparer);
 
@@ -164,7 +178,25 @@ baz
  ";
 		var tokens = AssertAsync.CompletesIn(1000, () => Lexer.LexSource(source).Tokens);
 
-		var expectedTokens = new[] {
+		var expectedTokens = TokenSequenceBuilder.Create()
+			.Trivia(" ")
+			.Add(SyntaxType.Identifier, "foo")
+			.Trivia(" ")
+			.Trivia("\r")
+			.Add(SyntaxType.NewlineToken, "\n")
+			.Trivia("\t")
+			.Add(SyntaxType.Identifier, "bar")
+			.Trivia("\t")
+			.Trivia("\t")
+			.Trivia("\r")
+			.Add(SyntaxType.NewlineToken, "\n")
+			.Add(SyntaxType.Identifier, "baz")
+			.Trivia("\r")
+			.Add(SyntaxType.NewlineToken, "\n")
+			.Trivia(" ")
+			.EOF();
+
+		/*var expectedTokens = new[] {
 			Token(SyntaxType.Identifier, 1, 3, "foo").WithLeadingTrivia(new[] {
 				Trivia(0, " ")
 			}),
@@ -187,7 +219,7 @@ baz
 			Token(SyntaxType.EndOfFile, TextSpan.Empty, "").WithLeadingTrivia(new[] {
 				Trivia(20, " "),
 			}),
-		};
+		};*/
 		SyntaxTokenEqualityComparer comparer = new(0);
 		Assert.Equal(expectedTokens, tokens, comparer);
 	}
