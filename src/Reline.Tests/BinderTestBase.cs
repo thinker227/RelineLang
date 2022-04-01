3a5a2968774a015e1e21f47b0b6bc838e2865e9b
thinker227 <thinker0227@gmail.com>
2022-04-02 01:39:57 +0200

    Added simple binder tests

---
+++ src/Reline.Tests/BinderTestBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Reline.Compilation.Parsing;
+using Reline.Compilation.Binding;
+using Reline.Compilation.Symbols;
+
+namespace Reline.Tests;
+
+public abstract class BinderTestBase : TreeTestBase<ISymbol> {
+
+	private IEnumerable<ISymbol>? enumerable;
+
+	/// <summary>
+	/// Sets the tree to traverse from a source string.
+	/// </summary>
+	/// <param name="source">The source stirng to generate the tree from.</param>
+	protected void SetTree(string source) {
+		var syntaxTree = AssertAsync.CompletesIn(2000, () => Parser.ParseString(source));
+		var semanticTree = AssertAsync.CompletesIn(2000, () => Binder.BindTree(syntaxTree));
+		var symbols = semanticTree.Root.GetDescendants();
+		enumerable = symbols
+			.Prepend(semanticTree.Root)
+			.Select(s => (ISymbol)s);
+	}
+
+	protected override IEnumerable<ISymbol>? GetEnumerable() =>
+		enumerable;
+	protected override void Reset() =>
+		enumerable = null;
+
+}
---
+++ src/Reline.Tests/Tests/BinderTests.cs
@@ -0,0 +1,52 @@
+﻿using Reline.Compilation.Symbols;
+
+namespace Reline.Tests;
+
+public class BinderTests : BinderTestBase {
+
+	[Fact]
+	public void ExpressionStatementFunctionInvocation() {
+		string source =
+@"Write (""Hello world!"")";
+		SetTree(source);
+
+		Node<ProgramSymbol>();
+		{
+			Node<LineSymbol>();
+			{
+				Node<ExpressionStatementSymbol>();
+				{
+					Node<FunctionInvocationExpressionSymbol>();
+					{
+						Node<LiteralExpressionSymbol>();
+					}
+				}
+			}
+		}
+		End();
+	}
+
+	[Fact]
+	public void SimpleLabelsAndVariables() {
+		string source =
+@"a: b = 0";
+		SetTree(source);
+
+		Node<ProgramSymbol>();
+		{
+			var line = Node<LineSymbol>();
+			Assert.NotNull(line.Label);
+			Assert.Equal("a", line.Label!.Identifier);
+			{
+				var assignment = Node<AssignmentStatementSymbol>();
+				Assert.NotNull(assignment.Variable);
+				Assert.Equal("b", assignment.Variable!.Identifier);
+				{
+					Node<LiteralExpressionSymbol>();
+				}
+			}
+		}
+		End();
+	}
+
+}
