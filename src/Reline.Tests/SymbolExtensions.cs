8fa63e727062a3fdc0b35b69d64d41727bda5632
thinker227 <thinker0227@gmail.com>
2022-04-02 14:02:13 +0200

    Add extensions and more binder tests

---
+++ src/Reline.Tests/SymbolExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Reline.Compilation.Symbols;
+
+namespace Reline.Tests;
+
+public static class SymbolExtensions {
+
+	public static LineSymbol LabelIs(this LineSymbol line, Action<LabelSymbol> action) {
+		Assert.NotNull(line.Label);
+		action(line.Label!);
+		return line;
+	}
+	public static AssignmentStatementSymbol VariableIs(this AssignmentStatementSymbol assignment, Action<IVariableSymbol> action) {
+		Assert.NotNull(assignment.Variable);
+		action(assignment.Variable!);
+		return assignment;
+	}
+	public static FunctionInvocationExpressionSymbol FunctionIs(this FunctionInvocationExpressionSymbol invocation, Action<IFunctionSymbol> action) {
+		Assert.NotNull(invocation.Function);
+		action(invocation.Function!);
+		return invocation;
+	}
+	public static IdentifierExpressionSymbol IdentifierIs(this IdentifierExpressionSymbol identifier, Action<IIdentifiableSymbol> action) {
+		Assert.NotNull(identifier.Identifier);
+		action(identifier.Identifier!);
+		return identifier;
+	}
+
+	public static TSymbol IdentifierIs<TSymbol>(this TSymbol identifiable, string identifier) where TSymbol : IIdentifiableSymbol {
+		Assert.Equal(identifier, identifiable.Identifier);
+		return identifiable;
+	}
+	public static TSymbol HasReferences<TSymbol>(this TSymbol identifiable, int count) where TSymbol : IDefinedIdentifiableSymbol {
+		Assert.Equal(count, identifiable.References.Count);
+		return identifiable;
+	}
+
+	public static LiteralExpressionSymbol HasValue(this LiteralExpressionSymbol literal, BoundValue value) {
+		Assert.Equal(value, literal.Literal);
+		return literal;
+	}
+
+	public static LineSymbol HasLineNumber(this LineSymbol symbol, int lineNumber) {
+		Assert.Equal(lineNumber, symbol.LineNumber);
+		return symbol;
+	}
+
+	public static T Do<T>(this T obj, Action action) {
+		action();
+		return obj;
+	}
+	public static T IsType<T>(this object? obj) {
+		Assert.NotNull(obj);
+		Assert.IsType<T>(obj);
+		return (T)obj!;
+	}
+	public static T Is<T>(this T obj, T other) {
+		Assert.Equal(obj, other);
+		return obj;
+	}
+
+}
--- src/Reline.Tests/Tests/BinderTests.cs
+++ src/Reline.Tests/Tests/BinderTests.cs
@@ -4,6 +4,22 @@ namespace Reline.Tests;
 
 public class BinderTests : BinderTestBase {
 
+	public void LineNumbers() {
+		int lines = 500;
+
+		string source = new string('\n', lines - 1);
+		var tree = SetTree(source);
+
+		Node<ProgramSymbol>();
+		{
+			for (int i = 0; i < lines; i++) {
+				Node<LineSymbol>()
+					.HasLineNumber(i + 1);
+			}
+		}
+		End();
+	}
+
 	[Fact]
 	public void ExpressionStatementFunctionInvocation() {
 		string source =
@@ -12,11 +28,14 @@ public class BinderTests : BinderTestBase {
 
 		Node<ProgramSymbol>();
 		{
-			Node<LineSymbol>();
+			Node<LineSymbol>()
+				.HasLineNumber(1);
 			{
 				Node<ExpressionStatementSymbol>();
 				{
-					Node<FunctionInvocationExpressionSymbol>();
+					Node<FunctionInvocationExpressionSymbol>()
+						.FunctionIs(f => f
+							.IdentifierIs("Write"));
 					{
 						Node<LiteralExpressionSymbol>();
 					}
@@ -36,15 +55,112 @@ public class BinderTests : BinderTestBase {
 
 		Node<ProgramSymbol>();
 		{
-			var line = Node<LineSymbol>();
-			Assert.NotNull(line.Label);
-			Assert.Equal("a", line.Label!.Identifier);
+			Node<LineSymbol>()
+				.HasLineNumber(1)
+				.LabelIs(l => l
+					.IdentifierIs("a"));
 			{
-				var assignment = Node<AssignmentStatementSymbol>();
-				Assert.NotNull(assignment.Variable);
-				Assert.Equal("b", assignment.Variable!.Identifier);
+				Node<AssignmentStatementSymbol>()
+					.VariableIs(v => v
+						.IdentifierIs("b"));
 				{
 					Node<LiteralExpressionSymbol>();
+				}
+			}
+		}
+		End();
+		
+		Assert.Empty(tree.Diagnostics);
+	}
+
+	[Fact]
+	public void SimpleLabelAndVariableReferences() {
+		string source =
+@"a: b = 2
+c: d = ""uwu""
+
+a
+b
+c
+d";
+		var tree = SetTree(source);
+
+		LabelSymbol a = null!;
+		IVariableSymbol b = null!;
+		LabelSymbol c = null!;
+		IVariableSymbol d = null!;
+
+		Node<ProgramSymbol>();
+		{
+			Node<LineSymbol>()
+				.HasLineNumber(1)
+				.LabelIs(l => l
+					.IdentifierIs("a")
+					.Do(() => a = l));
+			{
+				Node<AssignmentStatementSymbol>()
+					.VariableIs(v => v
+						.IdentifierIs("b")
+						.Do(() => b = v));
+				{
+					Node<LiteralExpressionSymbol>()
+						.HasValue(2);
+				}
+			}
+			Node<LineSymbol>()
+				.HasLineNumber(2)
+				.LabelIs(l => l
+					.IdentifierIs("c")
+					.Do(() => c = l));
+			{
+				Node<AssignmentStatementSymbol>()
+					.VariableIs(v => v
+						.IdentifierIs("d")
+						.Do(() => d = v));
+				{
+					Node<LiteralExpressionSymbol>()
+						.HasValue("uwu");
+				}
+			}
+			Node<LineSymbol>();
+			Node<LineSymbol>()
+				.HasLineNumber(4);
+			{
+				Node<ExpressionStatementSymbol>();
+				{
+					Node<IdentifierExpressionSymbol>()
+						.IdentifierIs(i => i
+							.Is(a));
+				}
+			}
+			Node<LineSymbol>()
+				.HasLineNumber(5);
+			{
+				Node<ExpressionStatementSymbol>();
+				{
+					Node<IdentifierExpressionSymbol>()
+						.IdentifierIs(i => i
+							.Is(b));
+				}
+			}
+			Node<LineSymbol>()
+				.HasLineNumber(6);
+			{
+				Node<ExpressionStatementSymbol>();
+				{
+					Node<IdentifierExpressionSymbol>()
+						.IdentifierIs(i => i
+							.Is(c));
+				}
+			}
+			Node<LineSymbol>()
+				.HasLineNumber(7);
+			{
+				Node<ExpressionStatementSymbol>();
+				{
+					Node<IdentifierExpressionSymbol>()
+						.IdentifierIs(i => i
+							.Is(d));
 				}
 			}
 		}
