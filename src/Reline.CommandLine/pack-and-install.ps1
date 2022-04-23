08f0af8cfc7c3ebe6cfdd8e51e8e516d752fea18
thinker227 <thinker0227@gmail.com>
2022-04-23 12:47:10 +0200

    Update install/update scripts to build in Release

--- src/Reline.CommandLine/pack-and-install.ps1
+++ src/Reline.CommandLine/pack-and-install.ps1
@@ -1,3 +1,4 @@
 # Script to pack and install the current version of the compiler CLI
-dotnet pack --no-restore
+dotnet build --configuration Release
+dotnet pack --no-build
 dotnet tool install --global --version 1.0.0-dev --add-source ./nupkg Reline.CommandLine
--- src/Reline.CommandLine/pack-and-update.ps1
+++ src/Reline.CommandLine/pack-and-update.ps1
@@ -1,3 +1,4 @@
 # Script to pack and update the current version of the compiler CLI
-dotnet pack --no-restore
+dotnet build --configuration Release
+dotnet pack --no-build
 dotnet tool update --global --version 1.0.0-dev --add-source ./nupkg Reline.CommandLine
