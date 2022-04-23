f64861a93746609f7d06b81264af3a4cd0178c70
thinker227 <thinker0227@gmail.com>
2022-04-23 12:26:11 +0200

    Create Statements.cs

---
+++ src/Reline.Tests/Tests/BinderTests/Statements.cs
@@ -0,0 +1,76 @@
+﻿using Reline.Compilation.Binding;
+using Reline.Compilation.Symbols;
+using static Reline.Tests.BinderTestUtilities;
+
+namespace Reline.Tests.BinderTests;
+
+public class Statements {
+
+	[Fact]
+	public void ManipulationStatements() {
+		string source =
+@"move 1..1 to 1
+swap 2..2 with 2
+copy 3..3 to 3";
+		var (r, model) = Compile(source);
+
+		r.Node<ProgramSymbol>();
+		{
+			r.Node<LineSymbol>()
+				.LineNumberIs(1);
+			{
+				r.Node<MoveStatementSymbol>();
+				{
+					r.Node<BinaryExpressionSymbol>()
+						.OperatorTypeIs(BinaryOperatorType.Range);
+					{
+						r.Node<LiteralExpressionSymbol>()
+							.HasValue(1);
+						r.Node<LiteralExpressionSymbol>()
+							.HasValue(1);
+					}
+					r.Node<LiteralExpressionSymbol>()
+						.HasValue(1);
+				}
+			}
+			r.Node<LineSymbol>()
+				.LineNumberIs(2);
+			{
+				r.Node<SwapStatementSymbol>();
+				{
+					r.Node<BinaryExpressionSymbol>()
+						.OperatorTypeIs(BinaryOperatorType.Range);
+					{
+						r.Node<LiteralExpressionSymbol>()
+							.HasValue(2);
+						r.Node<LiteralExpressionSymbol>()
+							.HasValue(2);
+					}
+					r.Node<LiteralExpressionSymbol>()
+						.HasValue(2);
+				}
+			}
+			r.Node<LineSymbol>()
+				.LineNumberIs(3);
+			{
+				r.Node<CopyStatementSymbol>();
+				{
+					r.Node<BinaryExpressionSymbol>()
+						.OperatorTypeIs(BinaryOperatorType.Range);
+					{
+						r.Node<LiteralExpressionSymbol>()
+							.HasValue(3);
+						r.Node<LiteralExpressionSymbol>()
+							.HasValue(3);
+					}
+					r.Node<LiteralExpressionSymbol>()
+						.HasValue(3);
+				}
+			}
+		}
+		r.End();
+
+		Assert.Empty(model.Diagnostics);
+	}
+
+}
